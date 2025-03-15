aecc7d1f8da749f6c00c1449bdeb75d1202da959
naomi <naomi.nagy01@gmail.com>
2025-03-15 18:42:48 +0200

    testing routing on production

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,14 @@ use App\Http\Middleware\AdminMiddleware;
 use App\Http\Controllers\UserController;
 
 
+Route::get('/', function () {
+    try {
+        return response()->file(public_path('index.html'));
+    } catch (\Exception $e) {
+        return response()->json(['error' => $e->getMessage()], 500);
+    }
+});
+
 // Public routes
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
